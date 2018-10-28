1265faead172039ca0d24c76b9ff50c02e9f15a8
kseniyakuznetsova <kseniya9696@gmail.com>
2018-10-28 16:14:11 +0400

    Initial commit

---
+++ src/main/java/com/kuznetsova/web/controller/LoginController.java
@@ -0,0 +1,38 @@
+package com.kuznetsova.web.controller;
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = { "/", "/index**" }, method = RequestMethod.GET)
+    public ModelAndView indexPage() {
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Custom Login Form");
+        model.addObject("message", "This is protected page!");
+        model.setViewName("index");
+        return model;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(@RequestParam(value = "error", required = false) String error, @RequestParam(value = "logout", required = false) String logout) {
+        ModelAndView model = new ModelAndView();
+        if (error != null)
+            model.addObject("error", "Invalid username and password!");
+        if (logout != null)
+            model.addObject("msg", "You've been logged out successfully.");
+        model.setViewName("login");
+        return model;
+    }
+}
